8fade5456dc9a004e1b1c4be8859173e3481d9ca
andrewtbiehl <70184461+andrewtbiehl@users.noreply.github.com>
2020-09-25 14:00:26 -0500

    Direction list order (#12)
    * Add test verification of direction list order
    * Specify in direction docs that list order preserved

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -7,3 +7,12 @@ def test_direction_returns_valid_direction():
     assert Direction.EAST.value == (0, 1)
     assert Direction.SOUTH.value == (1, 0)
     assert Direction.WEST.value == (0, -1)
+
+
+def test_list_returns_expected_direction_order():
+    assert list(Direction) == [
+        Direction.NORTH,
+        Direction.EAST,
+        Direction.SOUTH,
+        Direction.WEST,
+    ]
