b7091e5eb2aaddf59d60156251af268ef5d0e3ac
andrewtbiehl <70184461+andrewtbiehl@users.noreply.github.com>
2020-09-30 22:32:31 -0500

    Revert "Add index wrapper function to utility package (#24)"
    This reverts commit 9e7512d8eabbc8cf70072cc68042ed5a8f34552b.

--- gaslines/utility.py
+++ gaslines/utility.py
@@ -12,16 +12,3 @@ class Direction(Enum):
     EAST = (0, 1)
     SOUTH = (1, 0)
     WEST = (0, -1)
-
-
-def index(list_, value):
-    """
-    Returns the first index of the value if it is present and -1 otherwise.
-
-    This is a simple wrapper function for the list.index() method that, instead of
-    raising a ValueError if the value is absent, simply returns -1.
-    """
-    try:
-        return list_.index(value)
-    except ValueError:
-        return -1
