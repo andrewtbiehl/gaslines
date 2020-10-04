f9f8b2e126ab29b6204584f6d9cc9d64e91da838
andrewtbiehl <70184461+andrewtbiehl@users.noreply.github.com>
2020-10-04 16:18:01 -0500

    Add is_option function and tests (#27)

--- gaslines/solve.py
+++ gaslines/solve.py
@@ -1,3 +1,19 @@
+def is_option(current, neighbor):
+    """
+    Returns whether the neighbor is a valid option to be set as the child of current.
+    """
+    # Remaining segments that the neighbor would have as the child of current
+    neighbor_remaining_segments = (
+        current.remaining_segments - neighbor.is_on_different_segment(current)
+    )
+    return (
+        neighbor.is_open()
+        and neighbor_remaining_segments > 0
+        # Logically equivalent to "neighbor is sink implies one remaining segment"
+        and (not neighbor.is_sink() or neighbor_remaining_segments == 1)
+    )
+
+
 def get_head(grid):
     """
     Returns any head point on the grid if at least one exists, otherwise None.
--- tests/test_solve.py
+++ tests/test_solve.py
@@ -1,5 +1,5 @@
 from gaslines.grid import Grid
-from gaslines.solve import get_head, has_head
+from gaslines.solve import get_head, has_head, is_option
 
 
 def test_get_head_with_new_puzzle_returns_head():
@@ -36,3 +36,83 @@ def test_get_head_with_complete_puzzle_returns_none():
     grid[1][0].child = grid[2][0]
     assert not has_head(grid)
     assert get_head(grid) is None
+
+
+def test_is_option_with_open_neighbor_returns_true():
+    grid = Grid(((3, -1, -1), (-1, 2, -1), (0, -1, -1)))
+    current = grid[1][1]
+    assert is_option(current, grid[0][1])
+    assert is_option(current, grid[1][2])
+    assert is_option(current, grid[2][1])
+    assert is_option(current, grid[1][0])
+
+
+def test_is_option_with_unavailable_neighbor_returns_false():
+    grid = Grid(((3, -1, -1), (-1, 2, -1), (0, -1, -1)))
+    # Set partial path from "3"
+    grid[0][0].child = grid[0][1]
+    grid[0][1].child = grid[0][2]
+    grid[0][2].child = grid[1][2]
+    # Test neighbors on partial path from "3"
+    current = grid[1][1]
+    assert not is_option(current, grid[0][1])
+    assert not is_option(current, grid[1][2])
+
+
+def test_is_option_with_same_segment_and_one_remaining_segment_returns_true():
+    grid = Grid(((1, -1, -1, 0), (-1, -1, -1, -1)))
+    current = grid[0][1]
+    # Set partial path to current
+    grid[0][0].child = current
+    assert is_option(current, grid[0][2])
+
+
+def test_is_option_with_new_segment_and_one_remaining_segment_returns_false():
+    grid = Grid(((1, -1, -1, 0), (-1, -1, -1, -1)))
+    current = grid[0][1]
+    # Set partial path to current
+    grid[0][0].child = current
+    assert not is_option(current, grid[1][1])
+
+
+def test_is_option_with_same_segment_sink_and_one_remaining_segment_returns_true():
+    grid = Grid(((1, -1, 0), (-1, 0, -1)))
+    current = grid[0][1]
+    # Set partial path to current
+    grid[0][0].child = current
+    assert is_option(current, grid[0][2])
+
+
+def test_is_option_with_new_segment_sink_and_one_remaining_segment_returns_false():
+    grid = Grid(((1, -1, 0), (-1, 0, -1)))
+    current = grid[0][1]
+    # Set partial path to current
+    grid[0][0].child = current
+    assert not is_option(current, grid[1][1])
+
+
+def test_is_option_with_same_segment_sink_and_two_remaining_segments_returns_false():
+    grid = Grid(((2, -1, 0), (-1, 0, -1)))
+    current = grid[0][1]
+    # Set partial path to current
+    grid[0][0].child = current
+    assert not is_option(current, grid[0][2])
+
+
+def test_is_option_with_new_segment_sink_and_two_remaining_segments_returns_true():
+    grid = Grid(((2, -1, 0), (-1, 0, -1)))
+    current = grid[0][1]
+    # Set partial path to current
+    grid[0][0].child = current
+    assert is_option(current, grid[1][1])
+
+
+def test_is_option_with_sink_and_greater_than_two_remaining_segments_returns_false():
+    grid = Grid(((-1, 0, -1), (3, -1, 0), (-1, -1, 0)))
+    current = grid[1][1]
+    # Set partial path to current
+    grid[1][0].child = current
+    # Test sink on same segment
+    assert not is_option(current, grid[0][1])
+    # Test sink on new segment
+    assert not is_option(current, grid[1][2])
