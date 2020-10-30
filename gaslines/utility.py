44f7b8917c1b4a87bf30f355bbdd7189d0277ee6
andrewtbiehl <70184461+andrewtbiehl@users.noreply.github.com>
2020-10-29 20:50:16 -0700

    Redistribute print_and_backtrack responsibilities (#31)
    * Add get_number_of_rows function and tests
    * Revert "Add print_and_backtrack function and tests"

--- gaslines/utility.py
+++ gaslines/utility.py
@@ -12,3 +12,15 @@ class Direction(Enum):
     EAST = (0, 1)
     SOUTH = (1, 0)
     WEST = (0, -1)
+
+
+def get_number_of_rows(string):
+    """
+    Determines the number of rows (a.k.a. lines) contained in the given string. This
+    is equivalent to counting one more than the number of newline characters in the
+    string.
+
+    Args:
+        string (str): The string whose rows are to be counted.
+    """
+    return len(string.split("\n"))
--- tests/test_display.py
+++ tests/test_display.py
@@ -1,23 +1,5 @@
-from gaslines.display import Cursor, print_and_backtrack
+from gaslines.display import Cursor
 import pytest
-
-
-UNSOLVED_GRID_STRING = """\
-3   ·   ·
-         
-·   2   ·
-         
-*   ·   ·\
-"""
-
-
-SOLVED_GRID_STRING = """\
-3---·---·
-        |
-·---2   ·
-|       |
-*---·---·\
-"""
 
 
 # Note: these automated tests merely verify that the functions under test behave
@@ -40,12 +22,3 @@ def test_move_up_with_non_positive_number_does_nothing(capsys, number_of_rows):
 def test_clear_below_prints_correct_code(capsys):
     Cursor.clear_below()
     assert capsys.readouterr().out == "\x1b[J"
-
-
-@pytest.mark.parametrize(
-    "input_", ("", "test", UNSOLVED_GRID_STRING, SOLVED_GRID_STRING)
-)
-def test_print_and_backtrack_with_various_inputs_prints_correctly(capsys, input_):
-    lines = len(input_.split("\n"))
-    print_and_backtrack(input_)
-    assert capsys.readouterr().out == f"\x1b[J{input_}\n\x1b[{lines}A"
