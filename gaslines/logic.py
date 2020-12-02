7fed9cba3363f240506ed119407f7d60b07dccdc
Andrew Biehl <github@andrewtbiehl.email>
2020-12-01 20:48:15 -0800

    Remove duplicated 'solve' function and tests from 'logic' module

--- gaslines/logic.py
+++ gaslines/logic.py
@@ -1,10 +1,7 @@
 """
-Module that holds all utilities related to solving a Gas Lines puzzle. Most notably,
-this includes the various Gas Lines algorithms.
+Module that holds both the Strategy class, the container class for the various Gas
+Lines algorithms, and helper functions for said algorithms.
 """
-
-
-import gaslines.display as display
 
 
 class Strategy:
@@ -76,38 +73,6 @@ class Strategy:
                 return True
         current.child = None
         return False
-
-
-# Note: The solve function must appear below the Strategy class in the module in order
-# to avoid a NameError
-
-
-def solve(grid, strategy=Strategy.full_recursive, reveal_delay=None):
-    """
-    Solves a Gas Lines puzzle (using the strategy provided).
-
-    Mutates the grid object provided to search for a solution and returns True once a
-    solution has been found or False if no solution exists.
-
-    Args:
-        grid (Grid): A (presumably unsolved) Gas Lines grid.
-        strategy (Strategy class method): The choice of algorithm with which to solve
-            the grid. Defaults to the "full_recursive" strategy.
-        reveal_delay (float, NoneType): If not None, displays intermediate stages of
-            the search, pausing between each stage for the given number of seconds.
-            Defaults to None.
-
-    Returns:
-        bool: Whether the grid has a solution.
-    """
-    strategy_name = strategy.__name__
-    strategy_container = Strategy()
-    solve = getattr(strategy_container, strategy_name)
-    # Optionally decorate the algorithm with reveal functionality
-    if reveal_delay is not None:
-        solve = display.reveal(solve, reveal_delay)
-        setattr(strategy_container, strategy_name, solve)
-    return solve(grid)
 
 
 def get_next(current):
