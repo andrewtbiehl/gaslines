0ab7f7a905973c6b2a51896a6c872b658e057a2c
Andrew Biehl <70184461+andrewtbiehl@users.noreply.github.com>
2021-01-03 00:03:48 -0800

    Observer pattern implementation (#59)

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -3,7 +3,7 @@
 
 import pytest
 
-from gaslines.utility import Direction, get_number_of_rows
+from gaslines.utility import Direction, Observable, get_number_of_rows
 
 
 UNSOLVED_GRID_STRING = """\
@@ -13,6 +13,52 @@ UNSOLVED_GRID_STRING = """\
          
 *   ·   ·\
 """  # noqa: W293
+
+
+class Incrementor:
+    """
+    A simple class that initializes an integer value at zero and then, whenever
+    directed, increments that value by one.
+    """
+
+    def __init__(self):
+        self._count = 0
+
+    def increment(self):
+        """Increments the count."""
+        self._count += 1
+
+    @property
+    def count(self):
+        """Returns the current count."""
+        return self._count
+
+
+class Switch:
+    """
+    A simple class that initializes a boolean value to False and then, whenever
+    directed, negates that value.
+    """
+
+    def __init__(self):
+        self._state = False
+
+    def flip(self):
+        """Negates the current state."""
+        self._state = not self._state
+
+    @property
+    def state(self):
+        """Returns the current state."""
+        return self._state
+
+
+class MockDerivedObservable(Observable):
+    """A mock class used to test inheritance of the Observable class."""
+
+    @Observable.observe
+    def mock_observed_method(self):
+        """A mock method used to test the `observe` decorator functionality."""
 
 
 def test_direction_returns_valid_direction():
@@ -42,3 +88,31 @@ def test_get_number_of_rows_with_input_string_returns_expected_number(
 ):
     """Verifies that `get_number_of_rows` returns the expected number of rows."""
     assert get_number_of_rows(input_string) == expected_number
+
+
+@pytest.mark.parametrize(
+    "observable,method",
+    (
+        (Observable(), Observable.notify),
+        (MockDerivedObservable(), MockDerivedObservable.notify),
+        (MockDerivedObservable(), MockDerivedObservable.mock_observed_method),
+    ),
+)
+def test_observable_method_with_observers_notifies_observers(observable, method):
+    """Verifies that Observable instances can register and notify observers."""
+    # Instantiate two different example observers
+    incrementor = Incrementor()
+    switch = Switch()
+    # Register (some methods of) these observers with the observable
+    for observer in (incrementor.increment, switch.flip):
+        observable.register(observer)
+    # Notify the observers (directly or indirectly) an arbitrary number of times
+    # Verify that the observers react as expected to being notified
+    assert incrementor.count == 0
+    assert not switch.state
+    for i in range(1, 4):
+        old_state = switch.state
+        # Call the given method with respect to the given observable
+        method(observable)
+        assert incrementor.count == i
+        assert switch.state == (not old_state)
