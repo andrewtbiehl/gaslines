b76e226c0e81dfae162be201b9caed5586e4049b
Andrew Biehl <70184461+andrewtbiehl@users.noreply.github.com>
2021-01-26 17:36:06 -0800

    Convert algorithms into functions (#66)
    * Convert Strategy methods into stand-alone functions
    * Remove now unnecessary Strategy instantiation

--- gaslines/solve.py
+++ gaslines/solve.py
@@ -8,10 +8,10 @@ Lines puzzle.
 import functools
 
 import gaslines.display as display
-from gaslines.logic import Strategy
+from gaslines.logic import full_recursive
 
 
-def solve(grid, strategy=Strategy.full_recursive, reveal_delay=None):
+def solve(grid, strategy=full_recursive, reveal_delay=None):
     """
     Solves a Gas Lines puzzle (using the strategy provided).
 
@@ -20,8 +20,8 @@ def solve(grid, strategy=Strategy.full_recursive, reveal_delay=None):
 
     Args:
         grid (Grid): A (presumably unsolved) Gas Lines grid.
-        strategy (Strategy class method): The choice of algorithm with which to solve
-            the grid. Defaults to the "full_recursive" strategy.
+        strategy (function): The choice of algorithm with which to solve the grid.
+            Defaults to the "full_recursive" strategy.
         reveal_delay (float, NoneType): If not None, displays intermediate stages of
             the search, pausing between each stage for the given number of seconds.
             Defaults to None.
@@ -29,14 +29,11 @@ def solve(grid, strategy=Strategy.full_recursive, reveal_delay=None):
     Returns:
         bool: Whether the grid has a solution.
     """
-    strategy_name = strategy.__name__
-    strategy_container = Strategy()
-    solve = getattr(strategy_container, strategy_name)
-    # Optionally decorate the algorithm with reveal functionality
+    # Optionally reveal the grid while it is being solved
     if reveal_delay is not None:
         # Reveal the grid once after each mutation
         reveal = functools.partial(display.reveal, grid, reveal_delay)
         grid.register(reveal)
         # Also reveal the grid in its initial state, prior to solving it
         reveal()
-    return solve(grid)
+    return strategy(grid)
