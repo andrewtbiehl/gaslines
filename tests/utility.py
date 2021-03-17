326f439d8005579151966a63bdb79a2de6336560
Andrew Biehl <github@andrewtbiehl.email>
2021-03-16 18:06:44 -0700

    Add `pairwise` function to test utility module
    Please see
    https://more-itertools.readthedocs.io/en/stable/api.html#more_itertools.pairwise
    for more information.

--- tests/utility.py
+++ tests/utility.py
@@ -2,3 +2,24 @@
 Module that holds both general-purpose and gaslines-specific utilities used throughout
 the gaslines test package.
 """
+
+
+import itertools
+
+
+def pairwise(iterable):
+    """
+    Returns an iterator of paired items, overlapping, from the original.
+
+    This function is essentially identical to the function of the same name provided
+    by the more-itertools project.
+
+    Args:
+        iterable (Iterable): The iterable of objects to pair up.
+
+    Yields:
+        tuple: The next ordered pair of items from the given iterable.
+    """
+    first, second = itertools.tee(iterable)
+    next(second, None)
+    yield from zip(first, second)
